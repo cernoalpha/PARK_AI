8fbd740fedf67771e7cd208116e4727d8f4ae73a
cernoalpha <mirshahzadmubeen1@gmail.com>
2024-07-03 22:37:03 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+        <Button className='bg-gray-600 w-32 h-12'>Shaddy</Button>
+    </div>
+
+  )
+}
+
+export default App
